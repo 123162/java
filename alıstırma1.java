9358d5bcf946332c897ead01468fd859a84dcd76
RABİA ŞENLİK <55270779+123162@users.noreply.github.com>
2021-05-10 14:04:00 +0300

    Create alıstırma1.java

---
+++ alıstırma1.java
@@ -0,0 +1,16 @@
+paket dizilerlab;
+
+içe aktarma java.util.Arrays;
+
+public class DizilerLab {
+
+public static void main(String[] args) {
+    int[] numbers=new int[2];
+    numbers[0]=3;
+    numbers[1]=9;
+    numbers[2]=6;
+    
+    System.out.println(numbers.length);//diznin uzunlugu
+    System.out.println(Arrays.toString(numbers));
+}
+}
