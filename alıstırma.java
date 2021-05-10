07752da55b2723d7869cca5ef3aa501695bdbf28
RABİA ŞENLİK <55270779+123162@users.noreply.github.com>
2021-05-10 14:01:43 +0300

    Create alıstırma.java

---
+++ alıstırma.java
@@ -0,0 +1,42 @@
+java.util.Scanner'ı içe aktarın;
+
+public class StringLab {
+
+public static void main(String[] args) {
+   /* String str1="hello";
+    String str2="hello";
+    System.out.println(str1.equals(str2));//str2 yi karakter karakter okuyarak str1 ıle aynı mı dıye kontrol eder.
+    str2="HELLO";
+    System.out.println(str1.equalsIgnoreCase(str2));//karakterlerin buyukluk kucukluklerını es gecer.true olur.
+    
+            //STRİNG METHODS
+            
+    Scanner sn=new Scanner(System.in);
+    System.out.println("lütfen bir yazı giriniz:");
+    String line=sn.nextLine();
+    System.out.println(line.toLowerCase());//stringın tum karakterlerını KÜÇÜK olarak yazacak.
+    System.out.println(line.toUpperCase());//stringın tum karakterlerını BÜYÜK olarak yazacak.
+    System.out.println(line.charAt(3));//girdigimiz stringdeki 3. indisteki karakteri geri dondurucek.
+    System.out.println(line.length());//girdigimiz stringdeki karakter sayısını verıcek.
+    System.out.println(line.substring(3));//3.karakterden sonra yazdırıyor degerı
+    System.out.println(line.isEmpty());//girdigimiz stringin bos olup omadıgını gosteren method.
+    System.out.println(line.replace("ab", "wu"));//ab strıng degerını wu ıle degısıyor.
+    System.out.println(line.indexOf('a'));//a karakterinin index degerini bize döndericek.
+    System.out.println(line.lastIndexOf('a'));//a karakterinin indexini sondan arama ile bize vericek.
+    System.out.println(line.trim());//verilen strıngın basındakı ve sonundakı boslukları sılıp tekrardan donduren method.*/
+    
+           
+                       //STRİNG TOPLAMA İŞLEMİ
+                       
+    Scanner sn=new Scanner(System.in);                   
+    System.out.println("1. kelımeyı girin:");
+    String str1=sn.nextLine();
+    System.out.println("2.. kelımeyı girin:");
+    String str2=sn.nextLine();
+    System.out.println(str1+ " " +str2);
+    
+  
+    
+    
+    
+}
