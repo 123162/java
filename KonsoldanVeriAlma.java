b7d8de7453d78c601a73f6ddc5e3d0d6457558e7
RABİA ŞENLİK <55270779+123162@users.noreply.github.com>
2021-06-22 13:24:10 +0300

    Create KonsoldanVeriAlma.java

---
+++ KonsoldanVeriAlma.java
@@ -0,0 +1,28 @@
+
+package konsoldanverialma;
+
+import java.util.Scanner;
+
+public class KonsoldanVeriAlma {
+
+    public static void main(String[] args) {
+        Scanner sn=new Scanner(System.in);
+        /*System.out.println("a ve b degerini giriniz");
+        int a=sn.nextInt();
+        int b=sn.nextInt();
+        System.out.println("A degeri:"+a);
+        System.out.println("B degeri:"+b);
+        
+        System.out.println("double deger giriniz");
+        double c=sn.nextDouble();
+        System.out.println("C degeri:"+c);*/
+        
+        System.out.println("string deger giriniz");
+        String str=sn.nextLine();
+        System.out.println("str degeri:"+str);
+
+        
+        
+    }
+    
+}
