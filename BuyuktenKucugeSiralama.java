d8d4257a1de830c42784cb252ac444dbb1cfd9c5
RABİA ŞENLİK <55270779+123162@users.noreply.github.com>
2021-06-24 16:01:35 +0300

    Create BuyuktenKucugeSiralama.java

---
+++ BuyuktenKucugeSiralama.java
@@ -0,0 +1,40 @@
+
+package buyuktenkucugesiralama;
+
+import java.util.Scanner;
+
+public class BuyuktenKucugeSiralama {
+
+    public static void main(String[] args) {
+        Scanner sn=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("a değerini giriniz: ");
+        a=sn.nextInt();
+        System.out.println("b değerini giriniz: ");
+        b=sn.nextInt();
+        System.out.println("c değerini giriniz: ");
+        c=sn.nextInt();
+        
+        if((a>b)&& (a>c)){
+            if(b>c){
+                System.out.println("a>b>c");
+            }else{
+                System.out.println("a>c>b");
+            }   
+        }else if((b>a)&&(b>c)){
+            if(a>c){
+                System.out.println("b>a>c");
+            }else{
+                System.out.println("b>c>a");
+            }
+        }else{
+            if(a>b){
+                System.out.println("c>a>b");
+            }else{
+                System.out.println("c>b>a");
+            }
+        }
+        
+    }
+    
+}
