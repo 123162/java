8ae562c5958ca73420eb7dd68f2bdd750ef15008
RABİA ŞENLİK <55270779+123162@users.noreply.github.com>
2021-06-25 12:55:58 +0300

    Create CiftSayiBulma.java

---
+++ CiftSayiBulma.java
@@ -0,0 +1,20 @@
+
+package whileornek;
+
+public class WhileOrnek {
+
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=100){
+            if(i%2==0){
+                System.out.println("çift sayılar: "+i);
+                
+            }
+            i++;
+        }
+        
+    }
+    
+    
+}
+  
