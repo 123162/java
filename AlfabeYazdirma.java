f75e428349fbda70856cd1a70119d4381d3f8dff
RABİA ŞENLİK <55270779+123162@users.noreply.github.com>
2021-06-30 20:23:29 +0300

    Create AlfabeYazdirma.java

---
+++ AlfabeYazdirma.java
@@ -0,0 +1,15 @@
+
+package fordongusu;
+
+public class ForDongusu {
+
+   
+    public static void main(String[] args) {
+        int i=1;
+        for(char x='a';x<='z';x++){
+            System.out.println("alfabenin "+i+".harfi="+x);
+            i++;
+        }
+    }
+    
+}
