f2583a41f20e230753b4fb86cb2a2e062c51490a
RABİA ŞENLİK <55270779+123162@users.noreply.github.com>
2021-07-08 11:43:02 +0300

    Create Palindrom.java

---
+++ Palindrom.java
@@ -0,0 +1,30 @@
+
+package javaapplication35;
+
+
+public class JavaApplication35 {
+    
+    static boolean isPalindrom(int input){
+        int tmp,tersSayi=0,kalan;
+        tmp=input;
+        
+        while(tmp!=0){
+            kalan=tmp%10;
+            tersSayi=tersSayi*10+kalan;
+            tmp/=10;
+        }
+        if(input==tersSayi){
+            return true;
+            
+        }else{
+            return false;
+        }
+
+    }
+
+  
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(123321));
+    }
+    
+}
